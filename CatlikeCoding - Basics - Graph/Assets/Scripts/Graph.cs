08cd9858f6c36f0af044261b4291de6c6b3290d7
andrewyosh1 <coolasy@gmail.com>
2021-11-07 19:56:57 -0600

    Graph tutorial complete

--- CatlikeCoding - Basics - Graph/Assets/Scripts/Graph.cs
+++ CatlikeCoding - Basics - Graph/Assets/Scripts/Graph.cs
@@ -8,8 +8,10 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
-    [SerializeField, Range(10,100)]
+    [SerializeField, Range(10, 100)]
     int resolution = 10;
+
+    Transform[] points;
 
     void Awake()
     {
@@ -17,17 +19,28 @@ public class Graph : MonoBehaviour
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
-        { 
-            Transform point = Instantiate(pointPrefab);
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = ((i + 0.5f) * step - 1f);
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
 
+    }
 
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
     }
 
 }
